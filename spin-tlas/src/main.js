16e894c5204af9bcc12c551118a616a26f62e50e
Malar5717 <malarmariam613@gmail.com>
2025-07-18 15:27:29 +0530

    resolved issues from pulling

--- spin-tlas/src/main.js
+++ spin-tlas/src/main.js
@@ -6,4 +6,3 @@ setupGlobe(globeContainer);
 
 // Expose setTheme to HTML
 window.setTheme = setTheme;
-git@github.com:Malar5717/spint-las.gitgit@github.com:Malar5717/spint-las.gitgit@github.com:Malar5717/spint-las.gitgit pull origin main --allow-unrelated-histories
